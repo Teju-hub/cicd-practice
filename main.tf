091ec3a81a452a0d36650a5fe73a0f52793b6896
Tejaswini631 <tejaswininayak1996@gmail.com>
2026-04-08 22:49:09 +0530

    main.tf file updated with ami Id

--- main.tf
+++ main.tf
@@ -3,11 +3,12 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "k8s_node" {
-    ami           = "ami-0c55b159cbfafe1f0"
-    instance_type = "t3.micro"
+resource "aws_instance" "k8s-node" {
+  ami             = "ami-04680790a315cd58d"
+  instance_type   = "c7i-flex.large"
+  key_name        = "cicd-key"
 
-    tags = {
-        Name = "k8s-node"
-    }
+  tags = {
+    Name = "k8s-node"
+  }
 }
